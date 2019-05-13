d5cdc3111d97b456d54b3d12593511837baab1b4
Fabian Zabala <fabiandzp@live.com>
2019-05-13 16:42:55 -0500

    Initial commit

---
+++ tests/acceptance/navigation.feature
@@ -0,0 +1,8 @@
+Feature: test search filed
+  As a User
+  That can span a few lines
+
+  Scenario: Homepage can go to Contact
+    Given I am on the homepage
+    #When I click on the link with id "blog-link"
+    #Then I am on the blog page
---
+++ tests/acceptance/steps/navigation.py
@@ -0,0 +1,13 @@
+from behave import *
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('headless')
+
+use_step_matcher('re') #Nos permite recibir parametros de los Escenarios del archivo .feature
+
+@given('I am on the homepage')
+def step_impl(contex):
+    #browser = webdriver.Chrome() Normal
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser.get('https://www.google.com/')
